964856f018f80134c63a01a0ba042f899a4cc029
eeo <edenosja20@gmail.com>
2026-08-21 11:10:10 +0200

    Add customer financial profile procedure

---
+++ procedures/01_get_customer_financial_profile.sql
@@ -0,0 +1,65 @@
+USE NovaBankDB;
+GO
+
+-- =====================================================
+/* Create a stored procedure called:
+
+   sp_GetCustomerFinancialProfile
+
+   The procedure should receive:
+
+       @CustomerId INT
+
+   Return the financial profile only for the
+   requested customer.
+
+   Show:
+   customer_id
+   first_name
+   last_name
+   total_account_balance
+   total_transaction_volume
+   total_loan_amount
+
+   You may reuse:
+       vw_CustomerFinancialProfile
+
+   Example execution:
+
+       EXEC sp_GetCustomerFinancialProfile
+            @CustomerId = 10;
+
+*/
+-- =====================================================
+
+CREATE OR ALTER PROCEDURE sp_GetCustomerFinancialProfile
+    @CustomerId INT
+AS
+BEGIN
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM Customer
+        WHERE customer_id = @CustomerId
+    )
+    BEGIN
+        PRINT 'Customer does not exist.';
+        RETURN;
+    END;
+
+    SELECT
+        customer_id,
+        first_name,
+        last_name,
+        total_account_balance,
+        total_transaction_volume,
+        total_loan_amount
+    FROM vw_CustomerFinancialProfile
+    WHERE customer_id = @CustomerId;
+
+END;
+GO
+
+EXEC sp_GetCustomerFinancialProfile
+    @CustomerId = 999;
